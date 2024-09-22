cf0fdad19284336234579228de6b75a336231ed6
riddsep <ridoseptiawan00@gmail.com>
2024-09-22 20:01:05 +0700

    feat: Add continue section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Continue from "./components/Continue";
+
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Continue />
     </>
   );
 }
---
+++ src/components/Continue.jsx
@@ -0,0 +1,89 @@
+import "./Continue.css";
+
+function Continue() {
+  return (
+    <section className="section">
+      <h2 className="section__title">Melanjutkan Tonton Film</h2>
+      <div className="card__list">
+        <div className="card">
+          <img
+            src="./continues/film-1.png"
+            alt="film-1."
+            className="card__img"
+          />
+          <div className="card__info">
+            <div className="card__info__title">Dont Look Up</div>
+            <div className="card__info__rating">&#9733; 4.5/5</div>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            src="./continues/film-1.png"
+            alt="film-1."
+            className="card__img"
+          />
+          <div className="card__info">
+            <div className="card__info__title">A Man Called Otto</div>
+            <div className="card__info__rating">&#9733; 4.5/5</div>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            src="./continues/film-1.png"
+            alt="film-1."
+            className="card__img"
+          />
+          <div className="card__info">
+            <div className="card__info__title">Dont Look Up</div>
+            <div className="card__info__rating">&#9733; 4.5/5</div>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            src="./continues/film-1.png"
+            alt="film-1."
+            className="card__img"
+          />
+          <div className="card__info">
+            <div className="card__info__title">Dont Look Up</div>
+            <div className="card__info__rating">&#9733; 4.5/5</div>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            src="./continues/film-1.png"
+            alt="film-1."
+            className="card__img"
+          />
+          <div className="card__info">
+            <div className="card__info__title">Dont Look Up</div>
+            <div className="card__info__rating">&#9733; 4.5/5</div>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            src="./continues/film-1.png"
+            alt="film-1."
+            className="card__img"
+          />
+          <div className="card__info">
+            <div className="card__info__title">Dont Look Up</div>
+            <div className="card__info__rating">&#9733; 4.5/5</div>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            src="./continues/film-1.png"
+            alt="film-1."
+            className="card__img"
+          />
+          <div className="card__info">
+            <div className="card__info__title">Dont Look Up</div>
+            <div className="card__info__rating">&#9733; 4.5/5</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Continue;
