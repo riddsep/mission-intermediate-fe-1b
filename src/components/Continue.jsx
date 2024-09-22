eef60d9b8c93c98c3bfb38875628f0e4d11ab06a
riddsep <ridoseptiawan00@gmail.com>
2024-09-22 23:46:41 +0700

    feat: Merender film dari data.js untuk section melanjutkan tonton film

--- src/components/Continue.jsx
+++ src/components/Continue.jsx
@@ -1,90 +1,24 @@
 import "./Continue.css";
+import data from "../data/film";
 
 function Continue() {
+  const cardItems = data.map((item) => {
+    return (
+      <div className="card" key={item.id}>
+        <img src={item.src} alt={item.alt} className="card__img" />
+        <div className="card__info">
+          <div className="card__info__title">{item.title}</div>
+          <div className="card__info__rating">&#9733; {item.rating}/5</div>
+        </div>
+      </div>
+    );
+  });
   return (
     <section className="section">
       <h2 className="section__title">Melanjutkan Tonton Film</h2>
       <div className="wrapper">
         <img src="/images/arrow-left.svg" alt="" className="scroll-left" />
-        <div className="card__list">
-          <div className="card">
-            <img
-              src="./continues/film-1.png"
-              alt="film-1."
-              className="card__img"
-            />
-            <div className="card__info">
-              <div className="card__info__title">Dont Look Up</div>
-              <div className="card__info__rating">&#9733; 4.5/5</div>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              src="./continues/film-1.png"
-              alt="film-1."
-              className="card__img"
-            />
-            <div className="card__info">
-              <div className="card__info__title">A Man Called Otto</div>
-              <div className="card__info__rating">&#9733; 4.5/5</div>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              src="./continues/film-1.png"
-              alt="film-1."
-              className="card__img"
-            />
-            <div className="card__info">
-              <div className="card__info__title">Dont Look Up</div>
-              <div className="card__info__rating">&#9733; 4.5/5</div>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              src="./continues/film-1.png"
-              alt="film-1."
-              className="card__img"
-            />
-            <div className="card__info">
-              <div className="card__info__title">Dont Look Up</div>
-              <div className="card__info__rating">&#9733; 4.5/5</div>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              src="./continues/film-1.png"
-              alt="film-1."
-              className="card__img"
-            />
-            <div className="card__info">
-              <div className="card__info__title">Dont Look Up</div>
-              <div className="card__info__rating">&#9733; 4.5/5</div>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              src="./continues/film-1.png"
-              alt="film-1."
-              className="card__img"
-            />
-            <div className="card__info">
-              <div className="card__info__title">Dont Look Up</div>
-              <div className="card__info__rating">&#9733; 4.5/5</div>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              src="./continues/film-1.png"
-              alt="film-1."
-              className="card__img"
-            />
-            <div className="card__info">
-              <div className="card__info__title">Dont Look Up</div>
-              <div className="card__info__rating">&#9733; 4.5/5</div>
-            </div>
-          </div>
-        </div>
+        <div className="card__list">{cardItems}</div>
         <img src="/images/arrow-right.svg" alt="" className="scroll-right" />
       </div>
     </section>
---
+++ src/data/film.js
@@ -0,0 +1,45 @@
+const filmList = [
+  {
+    id: 1,
+    title: "Don't Look Up",
+    rating: 4.5,
+    src: "./continues/film-1.png",
+    alt: "Don't Look Up image",
+  },
+  {
+    id: 2,
+    title: "All Of Us Are Dead",
+    rating: 4.2,
+    src: "./continues/film-2.png",
+    alt: "All Of Us Are Dead image",
+  },
+  {
+    id: 3,
+    title: "Blue Lock",
+    rating: 4.8,
+    src: "./continues/film-3.png",
+    alt: "Blue Lock image",
+  },
+  {
+    id: 4,
+    title: "A Man Called Otto",
+    rating: 4.6,
+    src: "./continues/film-4.png",
+    alt: "A Man Called Otto image",
+  },
+  {
+    id: 5,
+    title: "Baymax",
+    rating: 4.1,
+    src: "./continues/film-5.png",
+    alt: "Baymax image",
+  },
+  {
+    id: 6,
+    title: "Spiderman: Miles Morales",
+    rating: 4.8,
+    src: "./continues/film-6.png",
+    alt: "Spiderman: Miles Morales image",
+  },
+];
+export default filmList;
