a63ea74baaf59c9e291ec7ddd503f7fc6bf58e99
riddsep <ridoseptiawan00@gmail.com>
2024-09-23 13:59:39 +0700

    feat: Add top rating film section

--- src/data/top-rating.js
+++ src/data/top-rating.js
@@ -1,47 +1,47 @@
 const topRating = [
   {
     id: 1,
-    src: "./top-rating/film-1.png",
+    src: "./images/top-rating/film-1.png",
     alt: "",
   },
   {
     id: 2,
-    src: "./top-rating/film-2.png",
+    src: "./images/top-rating/film-2.png",
     alt: "",
   },
   {
     id: 3,
-    src: "./top-rating/film-3.png",
+    src: "./images/top-rating/film-3.png",
     alt: "",
   },
   {
     id: 4,
-    src: "./top-rating/film-4.png",
+    src: "./images/top-rating/film-4.png",
     alt: "",
   },
   {
     id: 5,
-    src: "./top-rating/film-5.png",
+    src: "./images/top-rating/film-5.png",
     alt: "",
   },
   {
     id: 6,
-    src: "./top-rating/film-6.png",
+    src: "./images/top-rating/film-6.png",
     alt: "",
   },
   {
     id: 7,
-    src: "./top-rating/film-7.png",
+    src: "./images/top-rating/film-7.png",
     alt: "",
   },
   {
     id: 8,
-    src: "./top-rating/film-8.png",
+    src: "./images/top-rating/film-8.png",
     alt: "",
   },
   {
     id: 9,
-    src: "./top-rating/film-9.png",
+    src: "./images/top-rating/film-9.png",
     alt: "",
   },
 ];
