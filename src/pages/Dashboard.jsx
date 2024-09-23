4f7f0494fdc10aa498710a58a810fd380c58a97f
riddsep <ridoseptiawan00@gmail.com>
2024-09-23 23:48:23 +0700

    feat: minor changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,22 +1,20 @@
 import "../styles/Dashboard.css";
 import Button from "../components/atomic/Button";
-import Input from "../components/atomic/Input";
 function Dashboard() {
   return (
     <main className="container__dashboard">
-      <nav className="nav">
+      <nav className="nav__dashboard">
         <img src="/images/logo.svg" className="nav__logo" />
         <Button className={"btn--primary"} label={"Sign in"} />
       </nav>
-      <div className="hero">
-        <h1 className="hero__title">
+      <div className="hero__container">
+        <h1 className="hero__title__dashboard">
           Create unforgettable moments. Watch together now!
         </h1>
-        <p className="hero__paragraph">
+        <p className="hero__paragraph__dashboard">
           Sign up for a 30-day free trial. Watch free of charge!
         </p>
         <div className="hero__input-container">
-          <Input type={"text"} className={"input input--hero"} />
           <Button className={"btn--primary"} label={"Get started"} />
         </div>
       </div>
