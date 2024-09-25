6b2ab9e1af239cf98ff06d6a15147698694d12b0
riddsep <ridoseptiawan00@gmail.com>
2024-09-25 23:21:21 +0700

    fix: carousel

--- src/components/atomic/Carousel.jsx
+++ src/components/atomic/Carousel.jsx
@@ -1,11 +1,34 @@
 import "./Carousel.css";
+import useEmblaCarousel from "embla-carousel-react";
+import { useCallback } from "react";
 
 function Caraousel(props) {
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
   return (
-    <div className="wrapper">
-      <img src="/images/arrow-left.svg" alt="" className="scroll-left" />
-      <div className="card__list">{props.cardItems}</div>
-      <img src="/images/arrow-right.svg" alt="" className="scroll-right" />
+    <div className="embla">
+      <img
+        src="/images/arrow-left.svg"
+        alt=""
+        className="scroll-left embla__prev"
+        onClick={scrollPrev}
+      />
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla-container">{props.cardItems}</div>
+      </div>
+      <img
+        src="/images/arrow-right.svg"
+        alt=""
+        className="scroll-right embla__next"
+        onClick={scrollNext}
+      />
     </div>
   );
 }
