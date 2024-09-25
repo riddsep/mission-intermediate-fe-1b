77b4d2e0d50bd348132621b7956cbc8f0ba39f13
riddsep <ridoseptiawan00@gmail.com>
2024-09-25 22:23:34 +0700

    feat: Add page not found

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -1,20 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import LoginForm from "../pages/LoginForm";
 import RegisterForm from "../pages/RegisterForm";
 import Home from "../pages/Home";
+import PageNotFound from "../pages/PageNotFound";
+
+const routers = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    children: [
+      {
+        path: "/login",
+        element: <LoginForm />,
+      },
+      {
+        path: "/register",
+        element: <RegisterForm />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+]);
 
 const Routers = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/accounts/login" element={<LoginForm />} />
-        <Route path="/accounts/register" element={<RegisterForm />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={routers} />;
 };
-
 export default Routers;
